9d376f48e765f8df8ed560e89ae1e981193beaf0
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-07-15 11:16:06 +1200

    Delete filter_sam_file_size.sh

--- filter_sam_file_size.sh
+++
@@ -1,5 +0,0 @@
-module load SAMtools
-samtools view -H mapped_reads_Z37TGN_6_scaffold.sam | grep "^@SQ" | sed 's/^@SQ\t//' | cut -f 1,2 | sed 's/SN://g; s/LN://g' | awk '$2 > 2000'
-> all_scaffolds_greater_2000.txt
-
-samtools view -L all_scaffolds_greater_2000.txt -h mapped_reads_Z37TGN_6_scaffold.sam > filtered.sam

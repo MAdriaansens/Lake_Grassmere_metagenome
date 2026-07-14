7658a39f25948c2127659144e3c3130afba25725
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-07-15 11:32:41 +1200

    Update Intermediate_stepFilter2000_run_stats_BBmap.sh

--- Mapping_reads/Intermediate_stepFilter2000_run_stats_BBmap.sh
+++ Mapping_reads/Intermediate_stepFilter2000_run_stats_BBmap.sh
@@ -9,15 +9,27 @@
 
 #size filtering
 module load seqtk/1.4-GCC-13.3.0
+seqtk seq -L 2000 /home/mad149/Metagenome_grassmere/Assembl_3/Meta_spades_vanilla/spades_assembly_vanilla_Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}/contigs.fasta > Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_vanilla_Spades_2Kbsize_filtered_contig.fna
+seqtk seq -L 2000 /home/mad149/Metagenome_grassmere/Assembl_3/Meta_spades_vanilla/spades_assembly_vanilla_Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}/scaffolds.fasta > Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_vanilla_Spades_2Kbsize_filtered_scaffold.fna
 
-#seqtk seq -L 2000 /home/mad149/Metagenome_grassmere/Assembl_3/Meta_spades_isolate/spades_assembly_isolate_Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}/contigs.fasta > Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_isolate_Spades_2Kbsize_filtered_contig.fna
-#seqtk seq -L 2000 /home/mad149/Metagenome_grassmere/Assembl_3/Meta_spades_isolate/spades_assembly_isolate_Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}/scaffolds.fasta > Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_isolate_Spades_2Kbsize_filtered_scaffold.fna
+seqtk seq -L 2000 /home/mad149/Metagenome_grassmere/Assembl_3/Meta_spades_isolate/spades_assembly_isolate_Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}/contigs.fasta > Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_isolate_Spades_2Kbsize_filtered_contig.fna
+seqtk seq -L 2000 /home/mad149/Metagenome_grassmere/Assembl_3/Meta_spades_isolate/spades_assembly_isolate_Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}/scaffolds.fasta > Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_isolate_Spades_2Kbsize_filtered_scaffold.fna
+
+seqtk seq -L 2000 /home/mad149/Metagenome_grassmere/Assembl_3/Meta_spades_careful/spades_assembly_careful_Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}/contigs.fasta > Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_careful_Spades_2Kbsize_filtered_contig.fna
+seqtk seq -L 2000 /home/mad149/Metagenome_grassmere/Assembl_3/Meta_spades_careful/spades_assembly_careful_Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}/scaffolds.fasta > Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_careful_Spades_2Kbsize_filtered_scaffold.fna
+
+seqtk seq -L 2000 Metagenome_grassmere/Assembl_3/Megahit/Megahit_Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}/final.contigs.fa > Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_MegaHIT_2Kbsize_filtered_scaffold.fna
 
 module load BBMap/39.19-GCC-12.3.0
 
 #do statistics using BBMap
-stats.sh in=Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_vanilla_Spades_2Kbsize_filtered_contig.fna 2>&1 > Step5_BBMap/Z37TGN${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_MetaSpades_size_2kfiltered_BBmap_contig.txt
-stats.sh in=Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_vanilla_Spades_2Kbsize_filtered_contig.fna 2>&1 > Step5_BBMap/Z37TGN${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_MetaSpades_size_2kfiltered_BBmap_contig.txt
+stats.sh in=Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_isolate_Spades_2Kbsize_filtered_contig.fna 2>&1 > Step5_BBMap/Z37TGN${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_MetaSpades_size_2kfiltered_BBmap_contig.txt
+stats.sh in=Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_isolate_Spades_2Kbsize_filtered_contig.fna 2>&1 > Step5_BBMap/Z37TGN${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_MetaSpades_size_2kfiltered_BBmap_contig.txt
+
+stats.sh in=Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_careful_Spades_2Kbsize_filtered_scaffold.fna 2>&1 > Step5_BBMap/Z37TGN${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_MetaSpades_size_2kfiltered_BBmap_scaffold.txt
+stats.sh in=Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_careful_Spades_2Kbsize_filtered_scaffold.fna 2>&1 > Step5_BBMap/Z37TGN${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_MetaSpades_size_2kfiltered_BBmap_scaffold.txt
 
 stats.sh in=Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_vanilla_Spades_2Kbsize_filtered_scaffold.fna 2>&1 > Step5_BBMap/Z37TGN${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_MetaSpades_size_2kfiltered_BBmap_scaffold.txt
 stats.sh in=Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_vanilla_Spades_2Kbsize_filtered_scaffold.fna 2>&1 > Step5_BBMap/Z37TGN${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_MetaSpades_size_2kfiltered_BBmap_scaffold.txt
+
+stats.sh in=Step4_filter_size/2kb/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_MegaHIT_2Kbsize_filtered_scaffold.fna 2>&1 > Step5_BBMap/Z37TGN${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_MetaSpades_size_2kfiltered_MegaHit_contig.txt

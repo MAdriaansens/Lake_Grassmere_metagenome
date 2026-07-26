efe2c2ad9b9c081ad33ee10569530d117647e5f5
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-07-26 12:24:19 +1200

    Delete run_aragorn.sh

--- run_aragorn.sh
+++
@@ -1,9 +0,0 @@
-
-module load ARAGORN/1.2.41-foss-2023a
-
-for bin in bins/*/*/*/*.fna; do
-    entry=$(basename "$bin")
-    aragorn -t -o Aragorn/output_${entry}.txt $bin
-    grep 'tRNA-' Aragorn/output_${entry}.txt | grep -o '^[^(]*' > Aragorn/output_edit_${entry}.txt
-    rm Aragorn/output_${entry}.txt
-done

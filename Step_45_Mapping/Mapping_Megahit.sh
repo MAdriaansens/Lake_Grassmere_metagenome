05b0f656f73df3d5efd2c5b0473eee89008e73c4
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-07-26 12:07:12 +1200

    Create Mapping_Megahit.sh

---
+++ Step_45_Mapping/Mapping_Megahit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+#SBATCH --job-name      BBMap
+#SBATCH --time          48:00:00
+#SBATCH --mem           100GB
+#SBATCH --cpus-per-task 20
+#SBATCH --exclude=n[13-15]
+#SBATCH --error         slurm_output_BBmap/TrimmFastQC_%A-%a.err
+#SBATCH --output        slurm_output_BBmap/TrimmFastQC_%A-%a.out
+#SBATCH --array=13
+#SBATCH --array         0-16
+declare -a array=($(seq 1 16))
+
+module load seqtk/1.4-GCC-13.3.0
+#seqtk seq -L 500 Assembl_3/Megahit/Megahit_Z37TGN_13/final.contigs.fa > /home/mad149/Metagenome_grassmere/Step4_filter_size/Z37TGN_13_TrimmomaticJuly7_MegaHit_500b_size_filtered.fna
+mkdir Step6_map_reads/${array[$SLURM_ARRAY_TASK_ID]}_MegaHit
+
+module load BBMap/39.19-GCC-12.3.0
+module load SAMtools/1.21-GCC-13.3.0
+
+for i in {1..16}
+do
+    cd /home/mad149/Metagenome_grassmere/Step6_map_reads/${array[$SLURM_ARRAY_TASK_ID]}_MegaHit
+    mkdir ${array[$SLURM_ARRAY_TASK_ID]}_vs_$i
+    #make ref
+    cd /home/mad149/Metagenome_grassmere/Step6_map_reads/${array[$SLURM_ARRAY_TASK_ID]}_MegaHit/${array[$SLURM_ARRAY_TASK_ID]}_vs_$i
+    bbmap.sh threads=$SLURM_CPUS_PER_TASK ref=/home/mad149/Metagenome_grassmere/Step4_filter_size/500b/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_TrimmomaticJuly7_MegaHit_500b_size_filtered.fna
+    bbmap.sh threads=$SLURM_CPUS_PER_TASK minid=0.98 in=/home/mad149/Metagenome_grassmere/Trimm_2/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_sample_${array[$SLURM_ARRAY_TASK_ID]}_R1.qc.fastq.gz in2=/home/mad149/Metagenome_grassmere/Trimm_2/Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_sample_${array[$SLURM_ARRAY_TASK_ID]}_R2.qc.fastq.gz out=/home/mad149/Metagenome_grassmere/Step6_map_reads/${array[$SLURM_ARRAY_TASK_ID]}_MegaHit/${array[$SLURM_ARRAY_TASK_ID]}_vs_$i/mapped_reads_Z37TGN_${array[$SLURM_ARRAY_TASK_ID]}_vs_${i}_MegaHit_scaffold.sam bamscript=sort_bam.sh
+    sh sort_bam.sh
+done

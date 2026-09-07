e130157ec235ec736d724427e424eddd8a472b36
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2026-09-08 10:26:31 +1200

    Update Make_NR_DB_CPA.py

--- Retrieve_Homologs_of_interest/Make_NR_DB_CPA.py
+++ Retrieve_Homologs_of_interest/Make_NR_DB_CPA.py
@@ -1,7 +1,7 @@
 from Bio import SeqIO
 
 CPA_dict={}
-for record in SeqIO.parse('/home/mad149/chapter_meta_analysis/Protein/fl/All_CPA_types.fasta', 'fasta'):
+for record in SeqIO.parse('/home/mad149/chapter_meta_analysis/Protein/fl/All_CPA_types_added_detail_uncharacterized.fasta', 'fasta'):
     if str(record.seq) not in CPA_dict:
         id_list = [record.id]
         CPA_dict[str(record.seq)] = id_list
@@ -9,7 +9,7 @@ for record in SeqIO.parse('/home/mad149/chapter_meta_analysis/Protein/fl/All_CPA
         CPA_dict[str(record.seq)].append(record.id)
 
 print(len(CPA_dict))
-for record in SeqIO.parse('/home/mad149/Metagenome_grassmere/Retrieve_Homologs_across-contigs/MetaBat_isolate_postDrep_CPAs.fasta', 'fasta'):
+for record in SeqIO.parse('/home/mad149/Metagenome_grassmere/Retrieved_CPAseqs/retrieved_from_bins/CPA_sequences_vs_HQMQ_bins_filtered_fl.fasta', 'fasta'):
     if str(record.seq) not in CPA_dict:
         id_list = [record.id]
         CPA_dict[str(record.seq)] = id_list
@@ -17,7 +17,7 @@ for record in SeqIO.parse('/home/mad149/Metagenome_grassmere/Retrieve_Homologs_a
         CPA_dict[str(record.seq)].append(record.id)
 print(len(CPA_dict))
 
-for record in SeqIO.parse('/home/mad149/Metagenome_grassmere/Retrieve_Homologs_across-contigs/HMMalign/PF00999_CPA_vsCPA_contigs_hmmaligned.fasta', 'fasta'):
+for record in SeqIO.parse('/home/mad149/Metagenome_grassmere/Retrieved_CPAseqs/retrieved_from_contigs/CPA_sequences_from_contigs_prokka_translated_hmmaligned_filtered_fl.fasta', 'fasta'):
     if str(record.seq) not in CPA_dict:
         id_list = [record.id]
         CPA_dict[str(record.seq)] = id_list
@@ -29,7 +29,7 @@ exclusive_contig = 0
 in_bins = 0
 GTDB_count = 0
 for key in CPA_dict.keys():
-    if 'Metaspades_isolate' in CPA_dict[key][0]:
+    if 'contig' in CPA_dict[key][0]:
         exclusive_contig = exclusive_contig + 1
     elif 'metabat2_isolate_spades' in CPA_dict[key][0]:
         in_bins = in_bins+1
